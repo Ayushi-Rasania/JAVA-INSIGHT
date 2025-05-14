fc40bbf2a82fbc1022f0c80c8c5c3e95f6a052cf
AyushiRasania <ayushi12321@gmail.com>
2025-05-14 11:33:24 +0530

    Removed duplicate Module1 Core Java folder from repo

--- Module1 Core Java/CorePracticals/src/com/core/InvalidAgeException.java
+++
@@ -1,8 +0,0 @@
-package com.core;
-
-public class InvalidAgeException extends Exception{
-    // Constructor
-    public InvalidAgeException(String message) {
-        super(message); // Call the superclass constructor with the custom message
-    }
-}
